a2955a89aa79b7dbc4f3567ccbd3265ca213bcca
ccarrascal <carlos.carrascal@gmail.com>
2020-12-25 02:19:08 +0100

    Look and feel improvements

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import style from "./style.module.scss";
 const Header = (): JSX.Element => {
     return (
         <header className={style.header}>
-            <div className={style.logo}>ASCII Space</div>
+            <h1 className={style.title}>Ascii space</h1>
         </header>
     )
 }
